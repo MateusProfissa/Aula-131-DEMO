26e96acc39985ca0082b69eb1d6724eac506b47d
MateusProfissa <94137703+MateusProfissa@users.noreply.github.com>
2022-06-20 15:13:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var img = "";
+var objetcs = [];
+var model_status = "";
+
+function preload() {
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420); 
+    canvas.center(); 
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded); 
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+
+function modelLoaded() {
+    console.log("Modelo Carregado.");
+    model_status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objetcs = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (model_status != "") {
+        for (index = 0; index < objetcs.length; index++) {
+            document.getElementById("status").innerHTML = "Objeto Detectado.";
+            fill("#FF0000");
+            var percent = floor(objetcs[index].confidence * 100);
+            text(objetcs[index].label + " " + percente + "%", objetcs[index].x + 15, objetcs[index].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objetcs[index].x, objetcs[index].y, objetcs[index].width, objetcs[index].height);
+        }
+    }
+}
